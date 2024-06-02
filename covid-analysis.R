fa31f56a6624a0e86a6762aff7cd0a1de8d1b765
Sanja Petrovic E2 4/2023 <sanjica.petrovic7@gmail.com>
2024-06-02 18:19:43 +0200

    analyze number of cases by age group

--- covid-analysis.R
+++ covid-analysis.R
@@ -20,3 +20,20 @@ covid.clean <- covid.clean %>%
     !is.na(age_group) &
     current_status == "Laboratory-confirmed case"
   )
+
+age_group_order <- c("0 - 17 years", "18 to 49 years", "50 to 64 years", "65+ years")
+
+age_group_cases <- covid.clean %>%
+  group_by(age_group) %>%
+  summarise(case_count = n()) %>%
+  arrange(desc(age_group))
+
+age_group_cases_df <- collect(age_group_cases)
+age_group_cases_df$age_group <- factor(age_group_cases_df$age_group, levels = age_group_order)
+
+ggplot(age_group_cases_df, aes(x = reorder(age_group, -case_count), y = case_count)) +
+  geom_bar(stat = "identity") +
+  labs(title = "COVID-19 Cases by Age Group", x = "Age Group", y = "Number of Cases") +
+  theme_minimal() +
+  scale_x_discrete(limits = age_group_order)
+
