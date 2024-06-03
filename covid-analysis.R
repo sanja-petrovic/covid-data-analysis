f4237346fb55b145d8081d25d627560784d91759
Sanja Petrovic E2 4/2023 <sanjica.petrovic7@gmail.com>
2024-06-03 13:43:51 +0200

    graph outcome based on age_group and sex

--- covid-analysis.R
+++ covid-analysis.R
@@ -47,35 +47,18 @@ ggplot(age_group_cases_df, aes(x = reorder(age_group, -case_count), y = case_cou
 age_group_outcomes <- covid.clean %>%
   group_by(age_group) %>%
   summarise(case_count = n(),
-            death_count = sum(ifelse(death_yn == TRUE, 1, 0), na.rm = TRUE),
-            icu_count = sum(ifelse(icu_yn == TRUE, 1, 0), na.rm = TRUE),
-            hosp_count = sum(ifelse(hosp_yn == TRUE, 1, 0), na.rm = TRUE)) %>%
+            icu_count = sum(ifelse(icu_yn == TRUE, 1, 0), na.rm = TRUE)
+            ) %>%
   arrange(desc(case_count))
 
 age_group_outcomes_df <- collect(age_group_outcomes)
 
 age_group_outcomes_df$age_group <- factor(age_group_outcomes_df$age_group, levels = age_group_order)
 
-ggplot(age_group_outcomes_df, aes(x = case_count, y = death_count, label = age_group)) +
-  geom_point(size = 3) +
-  geom_text(vjust = -0.5, hjust = 0.5) +
-  labs(title = "COVID-19 Cases vs Deaths by Age Group", x = "Number of Cases", y = "Number of Deaths") +
-  theme_minimal() +
-  scale_y_continuous(labels = scales::comma) +
-  scale_x_continuous(labels = scales::comma)
-
 ggplot(age_group_outcomes_df, aes(x = case_count, y = icu_count, label = age_group)) +
   geom_point(size = 3) +
   geom_text(vjust = -0.5, hjust = 0.5) +
   labs(title = "COVID-19 Cases vs ICU admissions by Age Group", x = "Number of Cases", y = "Number of ICU admissions") +
-  theme_minimal() +
-  scale_y_continuous(labels = scales::comma) +
-  scale_x_continuous(labels = scales::comma)
-
-ggplot(age_group_outcomes_df, aes(x = case_count, y = hosp_count, label = age_group)) +
-  geom_point(size = 3) +
-  geom_text(vjust = -0.5, hjust = 0.5) +
-  labs(title = "COVID-19 Cases vs Hospitalizations by Age Group", x = "Number of Cases", y = "Number of Hospitalizations") +
   theme_minimal() +
   scale_y_continuous(labels = scales::comma) +
   scale_x_continuous(labels = scales::comma)
@@ -97,5 +80,44 @@ ggplot(ca_monthly_summary_df, aes(x = case_month, y = case_count)) +
   theme_minimal() +
   scale_x_date(breaks = seq(min(ca_monthly_summary_df$case_month), max(ca_monthly_summary_df$case_month), by = "6 month")) +
   scale_y_continuous(labels = scales::comma)
+
+covid.outcome_summary <- covid.clean %>%
+  filter(
+    sex %in% c("Male", "Female") & 
+      hosp_yn %in% c("Yes", "No") & 
+      icu_yn %in% c("Yes", "No") & 
+      death_yn %in% c("Yes", "No") &
+      symptom_status %in% c("Symptomatic", "Asymptomatic")
+  ) %>%
+  mutate(
+    outcome = case_when(
+      hosp_yn == "Yes" ~ "Hospitalized",
+      death_yn == "Yes" ~ "Death",
+      symptom_status == "Asymptomatic" ~ "Asymptomatic",
+      symptom_status == "Symptomatic" & hosp_yn != "Yes" & death_yn != "Yes" & icu_yn != "Yes" ~ "Symptomatic, not severe",
+      TRUE ~ "Other"
+    )
+  ) %>%
+  filter(outcome != "Other") %>%
+  group_by(age_group, sex, outcome)  %>%
+  summarise(count = n(), .groups = 'drop') %>%
+  group_by(age_group, sex) %>%
+  mutate(percent = count / sum(count) * 100)
+
+covid.outcome_summary_df <- collect(covid.outcome_summary)
+
+ggplot(covid.outcome_summary_df, aes(x = age_group, y = percent, fill = outcome)) +
+  geom_bar(stat = "identity", position = "fill", color = "white") +
+  facet_wrap(~sex) +
+  labs(title = "COVID-19 Laboratory-Confirmed Case Outcome by Age Group and Sex",
+       x = "Age Group",
+       y = "Percentage",
+       fill = "Outcome") +
+  theme_minimal() +
+  scale_fill_brewer(palette = "Set2") +
+  scale_x_discrete(limits = age_group_order) +
+  scale_y_continuous(labels = scales::comma)
+
+# CLASSIFICATION
 
 
